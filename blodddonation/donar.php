ef73488a1cf1eab5801da5338cdcfcdd86daf810
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-07-14 21:26:08 +0000

    Add files via upload

--- blodddonation/donar.php
+++ blodddonation/donar.php
@@ -3,10 +3,86 @@ include('connect.php');
 
 if(isset($_POST['submit'])){
     if(isset($_POST['term']) === true){
+        if(isset($_POST['name'])&&!empty($_POST['name'])){
+            if(preg_match('/^[A-Za-z\s]+$/',$_POST['name'])){
+                $name=$_POST['name'];
+            }
+            else{$nameerror='<div class="alert alert-danger alert dismissible fade show" role="alert">
+                <strong>Only lower and upper case and space are allowed</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times</span>
+                </button>
+                </div>';
+      }
+        }
+        else{
+            $nameerror='<div class="alert alert-danger alert dismissible fade show" role="alert">
+            <strong>fill the name field</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times</span>
+            </button>
+            </div>';
+        }
+        if(isset($_POST['gender'])&&!empty($_POST['gender'])){
+           
+                $gender=$_POST['gender'];
+            
+      }
         
-    }else{
+        else{
+            $gendererror='<div class="alert alert-danger alert dismissible fade show" role="alert">
+            <strong>fill the gender field</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times</span>
+            </button>
+            </div>';
+        }
+        if(isset($_POST['day'])&&!empty($_POST['day'])){
+           
+            $gender=$_POST['day'];
+        
+  }
+    
+    else{
+        $dayerror='<div class="alert alert-danger alert dismissible fade show" role="alert">
+        <strong>Please enter the valid day</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times</span>
+        </button>
+        </div>';
+    }
+    if(isset($_POST['month'])&&!empty($_POST['month'])){
+           
+        $gender=$_POST['month'];
+    
+}
+
+else{
+    $montherror='<div class="alert alert-danger alert dismissible fade show" role="alert">
+    <strong>Please select the valid month</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times</span>
+    </button>
+    </div>';
+}
+if(isset($_POST['year'])&&!empty($_POST['year'])){
+           
+    $gender=$_POST['year'];
+
+}
+
+else{
+$yearerror='<div class="alert alert-danger alert dismissible fade show" role="alert">
+<strong>Please select the valid year</strong>
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times</span>
+</button>
+</div>';
+}
+    }
+    else{
         $termerror='<div class="alert alert-danger alert dismissible fade show" role="alert">
-        <strong>Please agree with our terms and condition</strong>
+        <strong>Please agree our terms and conditions</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times</span>
         </button>
--- blodddonation/donate.php
+++ blodddonation/donate.php
@@ -23,7 +23,7 @@ include('donar.php')
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon" style="color: white;"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01" style="margin-left: 10%;">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01" >
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item ">
           <a class="nav-link" href="index.php" style="color: white;">Home </a>
@@ -69,6 +69,8 @@ include('donar.php')
                         <div class="form-group">
                             <label for="fullname">Full Name</label>
                             <input type="text" name="name" id="fullname" placeholder="Full Name" required pattern="[A-Za-z/\s]+" title="Only lower and upper case and space" class="form-control">
+                            <?php if(isset($nameerror)) echo $nameerror;
+                        ?>
                         </div><!--full name-->
                         <div class="form-group">
                   <label for="name">Blood Group</label><br>
@@ -88,20 +90,36 @@ include('donar.php')
                                   <label for="gender">Gender</label><br>
                                           Male<input type="radio" name="gender" id="gender" value="Male" style="margin-left:10px; margin-right:10px;" checked>
                                           Fe-male<input type="radio" name="gender" id="gender" value="Fe-male" style="margin-left:10px;">
+                                          <?php if(isset($gendererror)) echo $gendererror;
+                        ?>
                         </div><!--gender-->
                         <div class="form-inline">
-                  <label for="name">Date of Birth</label><br>
-                  <select class="form-control demo-default" id="date" name="date" style="margin-bottom:10px;" required>
+                  <label for="name">Date of Birth</label>
+                  <?php if(isset($dayerror)) echo $dayerror;
+                        ?>
+                        <?php if(isset($montherror)) echo $montherror;
+                        ?>
+                        <?php if(isset($yearerror)) echo $yearerror;
+                        ?>
+                  <br>
+                  <select class="form-control demo-default" id="day" name="day" style="margin-bottom:10px;" required>
                     <option value="">---Date---</option>
-                    <option value="01" >01</option><option value="02" >02</option><option value="03" >03</option><option value="04" >04</option><option value="05" >05</option><option value="06" >06</option><option value="07" >07</option> <option value="08" >08</option><option value="09" >09</option><option value="10" >10</option><option value="11" >11</option><option value="12" >12</option><option value="13" >13</option><option value="14" >14</option><option value="15" >15</option><option value="16" >16</option><option value="17" >17</option><option value="18" >18</option><option value="19" >19</option><option value="20" >20</option><option value="21" >21</option><option value="22" >22</option><option value="23" >23</option><option value="24" >24</option><option value="25" >25</option><option value="26" >26</option><option value="27" >27</option><option value="28" >28</option><option value="29" >29</option><option value="30" >30</option><option value="31" >31</option>
+                    <option value="01" >01</option><option value="02" >02</option><option value="03" >03</option><option value="04" >04</option><option value="05" >05</option><option value="06" >06</option><option value="07" >07</option> <option value="08" >08</option><option value="09" >09</option><option value="10" >10</option>
+                    <option value="11" >11</option><option value="12" >12</option><option value="13" >13</option><option value="14" >14</option><option value="15" >15</option><option value="16" >16</option><option value="17" >17</option><option value="18" >18</option><option value="19" >19</option><option value="20" >20</option>
+                    <option value="21" >21</option><option value="22" >22</option><option value="23" >23</option><option value="24" >24</option><option value="25" >25</option><option value="26" >26</option><option value="27" >27</option><option value="28" >28</option><option value="29" >29</option><option value="30" >30</option><option value="31" >31</option>
+                    
                   </select>
+                  
                   <select class="form-control demo-default" name="month" id="month" style="margin-bottom:10px;" required>
                     <option value="">---Month---</option>
                     <option value="01" >January</option><option value="02" >February</option><option value="03" >March</option><option value="04" >April</option><option value="05" >May</option><option value="06" >June</option><option value="07" >July</option><option value="08" >August</option><option value="09" >September</option><option value="10" >October</option><option value="11" >November</option><option value="12" >December</option>
                   </select>
                   <select class="form-control demo-default" id="year" name="year" style="margin-bottom:10px;" required>
                     <option value="">---Year---</option>
-                    <option value="1957" >1957</option><option value="1958" >1958</option><option value="1959" >1959</option><option value="1960" >1960</option><option value="1961" >1961</option><option value="1962" >1962</option><option value="1963" >1963</option><option value="1964" >1964</option><option value="1965" >1965</option><option value="1966" >1966</option><option value="1967" >1967</option><option value="1968" >1968</option><option value="1969" >1969</option><option value="1970" >1970</option><option value="1971" >1971</option><option value="1972" >1972</option><option value="1973" >1973</option><option value="1974" >1974</option><option value="1975" >1975</option><option value="1976" >1976</option><option value="1977" >1977</option><option value="1978" >1978</option><option value="1979" >1979</option><option value="1980" >1980</option><option value="1981" >1981</option><option value="1982" >1982</option><option value="1983" >1983</option><option value="1984" >1984</option><option value="1985" >1985</option><option value="1986" >1986</option><option value="1987" >1987</option><option value="1988" >1988</option><option value="1989" >1989</option><option value="1990" >1990</option><option value="1991" >1991</option><option value="1992" >1992</option><option value="1993" >1993</option><option value="1994" >1994</option><option value="1995" >1995</option><option value="1996" >1996</option><option value="1997" >1997</option><option value="1998" >1998</option><option value="1999" >1999</option>
+                    <option value="1957" >1957</option><option value="1958" >1958</option><option value="1959" >1959</option><option value="1960" >1960</option><option value="1961" >1961</option><option value="1962" >1962</option><option value="1963" >1963</option><option value="1964" >1964</option><option value="1965" >1965</option><option value="1966" >1966</option><option value="1967" >1967</option><option value="1968" >1968</option><option value="1969" >1969</option>
+                    <option value="1970" >1970</option><option value="1971" >1971</option><option value="1972" >1972</option><option value="1973" >1973</option><option value="1974" >1974</option><option value="1975" >1975</option><option value="1976" >1976</option><option value="1977" >1977</option><option value="1978" >1978</option>
+                    <option value="1979" >1979</option><option value="1980" >1980</option><option value="1981" >1981</option><option value="1982" >1982</option><option value="1983" >1983</option><option value="1984" >1984</option><option value="1985" >1985</option><option value="1986" >1986</option><option value="1987" >1987</option><option value="1988" >1988</option><option value="1989" >1989</option><option value="1990" >1990</option>
+                    <option value="1991" >1991</option><option value="1992" >1992</option><option value="1993" >1993</option><option value="1994" >1994</option><option value="1995" >1995</option><option value="1996" >1996</option><option value="1997" >1997</option><option value="1998" >1998</option><option value="1999" >1999</option>
                   </select>
                 </div><!--End form-group-->
                         <div class="form-group">
